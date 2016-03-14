97d693ad5950a5a99e6b7f3dbb10646650dc569a
Pr0gmaT1k <julien.cloupet@gmail.com>
2016-03-14 23:38:39 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -139,3 +139,5 @@ TW_THEME := portrait_hdpi
 TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness/
 TW_MAX_BRIGHTNESS := 255
 
+# Hack for build
+$(shell mkdir -p $(OUT)/obj/KERNEL_OBJ/usr)
